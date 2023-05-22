91cee7bc46f89b56a5a406daef77bfb06bf403be
Emerson78costad <emersoncostadantas1995@gmail.com>
2023-05-21 20:05:27 -0400

    completed2

--- src/components/TabelaDePostagem/TelaDePostagem.js
+++ src/components/TabelaDePostagem/TelaDePostagem.js
@@ -1,11 +1,10 @@
 import {ContainerPostagem,Image,Description} from "./styled"
-import img from "../../img/imagem.jpg"
 import React from "react";
 
 const TelaDePostagem =({imagem,descricao})=>{
     return(
         <ContainerPostagem>
-            <Image src={img} width="100%" height="80%"  alt={"drove veiw"}/>
+            <Image src={imagem} width="100%" height="80%"  alt={"drove veiw"}/>
             <Description>{descricao}</Description>
         </ContainerPostagem>
     )
